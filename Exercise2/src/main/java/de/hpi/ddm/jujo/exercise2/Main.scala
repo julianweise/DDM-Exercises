6879b1dce4b47401042149c70713ef1cf34a2666
Julian Weise <privat@julianweise.de>
2018-12-15 15:21:35 +0100

    refactoring main

--- Exercise2/src/main/java/de/hpi/ddm/jujo/exercise2/Main.scala
+++ Exercise2/src/main/java/de/hpi/ddm/jujo/exercise2/Main.scala
@@ -1,8 +1,10 @@
 package de.hpi.ddm.jujo.exercise2
 
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
-class Main extends App {
+import scala.collection.mutable.ListBuffer
+
+object Main extends App {
 
   def defineSparkSession(numberOfCores: Int): SparkSession = {
     val sparkBuilder = SparkSession
@@ -22,39 +24,40 @@ class Main extends App {
       .csv(pathToFile)
   }
 
-  def readCustomers(spark: SparkSession, pathToTPCH: String): Dataset[(Long, String, String, Int, String, Double, String, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH + "tpch_customer.csv").as[(Long, String, String, Int, String, Double, String, String)]
+  def readCustomers(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH + "tpch_customer.csv")
   }
 
-  def readLineItems(spark: SparkSession, pathToTPCH: String): Dataset[(Long, Long, Long, Long, Double, Double, Double, Double, Char, Char, String, String, String, String, String, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH + "tpch_lineitem.csv").as[(Long, Long, Long, Long, Double, Double, Double, Double, Char, Char, String, String, String, String, String, String)]
+  def readLineItems(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH + "tpch_lineitem.csv")
   }
 
-  def readNations(spark: SparkSession, pathToTPCH: String): Dataset[(Long, String, Long, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH  + "tpch_nation.csv").as[(Long, String, Long, String)]
+  def readNations(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH  + "tpch_nation.csv")
   }
 
-  def readOrders(spark: SparkSession, pathToTPCH: String): Dataset[(Long, Long, Char, Double, String, String, String, Long, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH  + "tpch_orders.csv").as[(Long, Long, Char, Double, String, String, String, Long, String)]
+  def readOrders(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH  + "tpch_orders.csv")
   }
 
-  def readParts(spark: SparkSession, pathToTPCH: String): Dataset[(Long, String, String, String, String, Long, String, Double, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH  + "tpch_part.csv").as[(Long, String, String, String, String, Long, String, Double, String)]
+  def readParts(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH  + "tpch_part.csv")
   }
 
-  def readRegions(spark: SparkSession, pathToTPCH: String): Dataset[(Long, String, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH + "tpch_region.csv").as[(Long, String, String)]
+  def readRegions(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH + "tpch_region.csv")
   }
 
-  def readSuppliers(spark: SparkSession, pathToTPCH: String): Dataset[(Long, String, String, Long, String, Double, String)] = {
-    import spark.implicits._
-    readFromCSV(spark, pathToTPCH  + "tpch_supplier.csv").as[(Long, String, String, Long, String, Double, String)]
+  def readSuppliers(spark: SparkSession, pathToTPCH: String): DataFrame = {
+    readFromCSV(spark, pathToTPCH  + "tpch_supplier.csv")
+  }
+
+  def extractColumnsFromRow(columnNames: Array[String], row: Row): List[(String, String)] = {
+    var tuples = new ListBuffer[(String, String)]()
+    for(i <- columnNames.length) {
+      tuples += ((columnNames.apply(i), row.getString(i)))
+    }
+    tuples.toList
   }
 
   override def main(args: Array[String]): Unit = {
@@ -74,9 +77,14 @@ class Main extends App {
     }
 
     val sparkSession = defineSparkSession(numberOfCores)
+    import sparkSession.implicits._
 
     val nations = readNations(sparkSession, pathToTPCH)
     val regions = readRegions(sparkSession, pathToTPCH)
+
+    nations
+      .flatMap(row => extractColumnsFromRow(nations.columns, row))
+      .show()
 
   }
 }
